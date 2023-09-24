69a9545743e7e790885aa17b2c42421dba482f65
Rhichard Koh <82534598+ROCCYK@users.noreply.github.com>
2023-09-24 15:59:53 -0400

    added code 1 and 2

---
+++ CODE/Code1/2. Intro to Tensorboard.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 13 08:29:50 2023
+
+@author: LZ8JXZ
+"""
+
+"""
+Using....
+https://archive.ics.uci.edu/ml/datasets/Breast+Cancer+Wisconsin+(Diagnostic)
+
+"""
+
+import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
+ 
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation, Dropout
+
+df = pd.read_csv("breast_cancer_data.csv")
+
+print(df.describe().T)  #Values need to be normalized before fitting. 
+print(df.isnull().sum())
+
+#Rename Dataset to Label to make it easy to understand
+df = df.rename(columns={'Diagnosis':'Label'})
+print(df.dtypes)
+
+#Replace categorical values with numbers
+df['diagnosis'].value_counts()
+
+#Define the dependent variable that needs to be predicted (labels)
+y = df["diagnosis"].values
+
+# Encoding categorical data
+from sklearn.preprocessing import LabelEncoder
+labelencoder = LabelEncoder()
+Y = labelencoder.fit_transform(y) # M=1 and B=0
+#################################################################
+
+#Define x and normalize values
+#Define the independent variables. Let's also drop Gender, so we can normalize other data
+X = df.drop(labels = ["diagnosis", "id"], axis=1) 
+
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler()
+scaler.fit(X)
+X = scaler.transform(X)
+
+#Split data into train and test to verify accuracy after fitting the model. 
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.25, random_state=42)
+
+
+####################################################################
+#Simple Network
+model = Sequential()
+model.add(Dense(16, input_dim=30, activation='relu')) 
+model.add(Dropout(0.2))
+model.add(Dense(1)) 
+model.add(Activation('sigmoid'))  
+model.compile(loss='binary_crossentropy',
+              optimizer='adam',            
+              metrics=['accuracy'])
+print(model.summary())
+
+###########################################################
+#Tensorboard callback
+
+from tensorflow.keras.callbacks import TensorBoard
+file_name = 'my_saved_model'  #Give unique name each time, otherwise results get concatenated
+#Try running multiple times and see the output in tensorboard
+#If file name is changed, you can select the specific model in TensorBoard
+
+tensorboard = TensorBoard(log_dir="C:/Users/Downloads/COSC/logs\\{}".format(file_name))
+
+#After training open Anaconda command prompt and type the following:
+# python -m tensorboard.main --logdir=logs/
+# Gives the URL that you copy and paste in a browser.
+
+# # evaluate the model
+history = model.fit(X_train, y_train ,verbose=1, epochs=50, batch_size=64,
+                     validation_data=(X_test, y_test), callbacks=[tensorboard])
+
+
+# Predict
+_, acc = model.evaluate(X_test, y_test)
+print("Accuracy = ", (acc * 100.0), "%")
+
+
+# prediction_test = model.predict(X_test)    
+# print(y_test, prediction_test)
+# print("Mean sq. errror between y_test and predicted =", np.mean(prediction_test-y_test)**2)
+
+
+#plot the training and validation accuracy and loss at each epoch
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(1, len(loss) + 1)
+plt.plot(epochs, loss, 'y', label='Training loss')
+plt.plot(epochs, val_loss, 'r', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+plt.plot(epochs, acc, 'y', label='Training acc')
+plt.plot(epochs, val_acc, 'r', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.show()
+
+
+# Predicting the Test set results
+y_pred = model.predict(X_test)
+y_pred = (y_pred > 0.5)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+sns.heatmap(cm, annot=True)
